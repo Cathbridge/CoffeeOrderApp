a83d64918de4a10349d2b51e853ee11e25f942d2
Cathbridge <catmlangeni@gmail.com>
2018-06-29 09:54:46 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,69 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+name: string;
+order: number;
+valueOne : number;
+whipped : boolean;
+wCoffee : string;
+chocolate: boolean;
+chocCoffee: string;
 
+price: number;
+
+  constructor(public navCtrl: NavController) {
+    this.order= 0;
+      this.valueOne = 0;
+      this.price = 10;
   }
+
+  onOrder(){
+    this.price = this.price * this.valueOne;
+
+    if(this.name == null){
+      alert("Please enter your name ")
+
+    }else if(this.valueOne == 0){
+      alert("Please enter quantity")
+    }
+
+    if(this.whipped == true){
+      // console.log(this.wCoffee)
+      this.price += 5;
+      this.wCoffee = "+Whipped Cream";
+    }
+     if(this.chocolate == true){
+      this.price += 2;
+      this.chocCoffee = "Chocolate";
+    }
+   
+
+    if(this.valueOne!= 0){
+    this.order =1;
+    }
+    
+      
+  }
+
+  onAdd(num: Number){
+    
+    if(num === 1){
+    this.valueOne += 1;
+    }else if(num=== 0 && this.valueOne > 0) 
+    { this.valueOne -= 1;
+      
+    }
+  }
+
+  onReset(){
+    this.valueOne=0;
+    this.whipped= false;
+    this.chocolate= false;
+    this.price=0;
+    this.name= "";
+  }
+
+
+
 
 }
